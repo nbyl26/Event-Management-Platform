65a841565a1277166080fde1343780ae021d490c
Nabil <nabilpasha230606@gmail.com>
2024-12-28 19:21:45 +0700

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
+      },
+      colors: {
+        primary: "#4F46E5",
+        accent: "#22C55E",
+      },
+    },
+  },
+  plugins: [],
+};
